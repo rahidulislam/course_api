fd19f6f056aa5d6733ef5cea48ef99bc1bac69b6
Rahidul Islam <rahiseli@outlook.com>
2022-03-04 13:18:57 +0600

    course and enrollment model create

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+from multiprocessing import parent_process
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    max_student = models.IntegerField(default=0)
+    statr_date = models.DateField()
+    end_date = models.DateField()
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+        
+class Enrollment(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_enroll')
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='course_student')
